6b6e09652ad401391e298456fbacb644b8d1f309
Tiraj KC <kctiraj44@gmail.com>
2022-07-05 15:05:30 -0500

    Rest API with full Database

---
+++ src/test/java/com/example/springbootfull/SpringBootFullwithDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFullwithDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
